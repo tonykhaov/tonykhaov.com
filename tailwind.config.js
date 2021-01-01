b9f561b9e2e01a95bea940b4a44b73578a921d95
Tony Khaov <tony.khaov@gmail.com>
2021-01-01 14:48:06 +0100

    fix: 🐛 tailwindcss purge to work with ts(x) files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./pages/**/*.js', './components/**/*.js'],
+  purge: ['./src/components/**/*.{js,ts,jsx,tsx}', './src/pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
