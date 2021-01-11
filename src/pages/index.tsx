2ea4c85065b0b182c241b4eabd607473eea11b68
Tony Khaov <tony.khaov@gmail.com>
2021-01-11 23:19:06 +0100

    fix: 🐛 use getStaticProps for index.tsx (#5)
    ✅ Closes: 6

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,7 +47,7 @@ export default function Home({ posts }: Props): JSX.Element {
   )
 }
 
-export async function getServerSideProps(): Promise<{ props: { posts: Post[] } }> {
+export async function getStaticProps() {
   const posts: Post[] = [buildPost(), buildPost(), buildPost(), buildPost(), buildPost()]
 
   return {
