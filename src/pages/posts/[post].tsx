93969c72503956c8ae5af6c7c7220535a8e1ca9f
Tony Khaov <tony.khaov@gmail.com>
2021-01-19 19:49:44 +0100

    feat: 🎸 integrate minimal Post page (#9)
    ✅ Closes: 13

--- src/pages/posts/[post].tsx
+++ src/pages/posts/[post].tsx
@@ -1,15 +1,23 @@
 import Header from 'src/components/screens/Header'
-import { useRouter } from 'next/dist/client/router'
+import { buildPost } from 'src/test/generate'
 
 export default function Post(): JSX.Element {
-  const { query } = useRouter()
+  const post = buildPost()
+
   return (
     <>
       <Header />
-      <main className="flex items-center justify-center h-96">
-        <h1 className="text-2xl">
-          Post #<span className="text-gray-400">{query.post}</span>
-        </h1>
+      <main>
+        <div className="container h-full p-4 mx-auto">
+          <h1 className="mb-2 text-2xl font-semibold text-center">{post.title}</h1>
+          <div className="mb-4 flex justify-center h-96">
+            <img
+              src="https://images.unsplash.com/photo-1610788696751-07e118e9fe01?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
+              alt={post.title}
+            />
+          </div>
+          <p className="text-lg leading-8">{post.content}</p>
+        </div>
       </main>
     </>
   )
