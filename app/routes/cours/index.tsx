be640c300e5361836a06ecd08e96f90cd81bae3c
Tony Khaov <tony.khaov@gmail.com>
2021-12-08 17:21:13 +0100

    feat: add cours summary

--- app/routes/cours/index.tsx
+++ app/routes/cours/index.tsx
@@ -1,5 +1,19 @@
+import { Link } from 'remix'
+
 function CoursIndex() {
-  return <h1>Good</h1>
+  return (
+    <>
+      <h1 className="text-xl">Cours</h1>
+      <ul className="ml-2">
+        <h2 className="text-lg underline">Maths</h2>
+        <li className="ml-2">
+          <Link to="signes" className="text-blue-700 hover:text-blue-500">
+            Les signes (négatifs)
+          </Link>
+        </li>
+      </ul>
+    </>
+  )
 }
 
 export default CoursIndex
